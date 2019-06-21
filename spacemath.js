80fe0ea724943db982c12a5e81f995c9caec42b0
anokata <anokata8@mail.ru>
2019-06-21 14:06:25 +0300

    buttons

--- spacemath.js
+++ spacemath.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", init);
 var ctx;
 var width, height;
+var ui={};
 const CLEAR_COLOR = "#777";
 
 function init() {
@@ -14,7 +15,17 @@ function init() {
     requestAnimationFrame(drawAll);
     a=0;
     ctx.font = "20px mono";
+    initUI();
 }
+
+function initUI() {
+    ui.buttons = [];
+    makeButton(100, 100, 130, 30, 'Начать', 'blue', 'red');
+    makeButton(300, 100, 130, 30, '1Начать', 'red', 'red');
+    makeButton(100, 300, 130, 30, '2Начать', 'green', 'red');
+    makeButton(300, 300, 130, 30, '3Начать', 'black', 'red');
+}
+
 function clear() {
     ctx.fillStyle = CLEAR_COLOR;
     ctx.fillRect(0,0,width, height);
@@ -28,5 +39,31 @@ function draw() {
     a += 1;
     ctx.fillStyle = "#faf";
     ctx.fillRect(10,10,10+a,20);
-    ctx.fillText("Начало", 20,120);
+
+    drawButtons();
+}
+
+function drawButtons() {
+    ui.buttons.forEach(function (b) {
+        ctx.fillStyle = b.color;
+        ctx.textAlign = "center";
+        ctx.fillRect(b.x, b.y, b.w, b.h);
+        ctx.fillStyle = "#fff";
+        let cx = b.x + b.w/2;
+        let cy = b.y + b.h/2 + 5;
+        ctx.fillText(b.label, cx, cy);
+    });
+}
+
+function makeButton(x, y, w, h, label, color, activeColor) {
+    let button = {
+        'x': x,
+        'y': y,
+        'w': w,
+        'h': h,
+        'label': label,
+        'color': color,
+        'activeColor': activeColor,
+    };
+    ui.buttons.push(button);
 }
